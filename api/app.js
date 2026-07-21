0dab56f071b2fa558ddbf0d391af983a85b8d971
c1felode <abdusamadov.workit@gmail.com>
2026-07-21 23:41:23 +0500

    auth/register logic added and prisma initialized

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
-import express from "express"
-import authRoute from "./routes/auth.route.js"
-
+import express from 'express'
+import authRouter from './routes/auth.route.js'
 
 const app = express()
 
-app.use('/api/auth', authRoute)
+app.use(express.json()) 
 
-app.listen(8800, ()=>{
-    console.log("server is running!")
+app.use('/api/auth', authRouter)
+
+const PORT = 8800
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`)
 })
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,24 @@
-export const register = (req, res) => {
-    // db operations
+import bcrypt from 'bcrypt'
+
+export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        console.log(hashedPassword)
+
+        // [Тут в будущем будет сохранение в базу данных]
+
+        return res.status(201).json({ 
+            message: "Пользователь успешно зарегистрирован!" 
+        })
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: "Внутренняя ошибка сервера" })
+    }
 }
+
 
 export const login = (req, res) => {
     // db operations
