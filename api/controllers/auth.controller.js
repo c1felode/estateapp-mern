b0922cf833edc4f33ca65e257d49047ee966f0df
c1felode <abdusamadov.workit@gmail.com>
2026-07-21 16:24:06 +0500

    api auth setup

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+    // db operations
+}
+
+export const login = (req, res) => {
+    // db operations
+}
+
+export const logout = (req, res) => {
+    // db operations
+}
