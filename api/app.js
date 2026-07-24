b6f0b735d3ae019040c44e75758906dac05d12cc
c1felode <abdusamadov.workit@gmail.com>
2026-07-24 23:51:17 +0500

    jwt added to login method

--- api/app.js
+++ api/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+import 'dotenv/config'; 
 
 const app = express()
 
+app.use(cookieParser())
 app.use(express.json()) 
 
 app.use('/api/auth', authRouter)
