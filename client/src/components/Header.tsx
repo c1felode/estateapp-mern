1b1377a372742921c762ade05e4ddf68c773d06a
c1felode <abdusamadov.workit@gmail.com>
2026-07-29 23:54:54 +0500

    UserInfo component added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
             </nav>
             <div>
                 <MiniProfile/>
-                <AuthActions/>
+                {/* <AuthActions/> */}
             </div>
         </header>
     )
